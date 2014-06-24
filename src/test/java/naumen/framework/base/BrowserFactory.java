cd1f2b428824ec9d9116333a0c4f219d14451ac2
vivshin <vivshin@naumen.ru>
2014-06-24 18:20:01 +0600

    tc

--- src/test/java/naumen/framework/base/BrowserFactory.java
+++ src/test/java/naumen/framework/base/BrowserFactory.java
@@ -31,7 +31,7 @@ public abstract class BrowserFactory {
 		switch (type) {
 		case CHROME:
 			cp = DesiredCapabilities.chrome();
-			cp.setCapability("chrome.switches", Arrays.asList("--disable-popup-blocking"));
+			cp.setCapability("chrome.switches", Arrays.asList("--no-sandbox", "--disable-popup-blocking"));
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("lang=ru");
 			cp.setCapability(ChromeOptions.CAPABILITY, options);
