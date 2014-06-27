a8a321b2a100f04560441118aad11c8c06dfd3d5
vivshin <vivshin@naumen.ru>
2014-06-27 16:34:31 +0600

    LOT-S 2nd part ready

--- src/test/java/naumen/tests/lot/LOTSLotSubscription.java
+++ src/test/java/naumen/tests/lot/LOTSLotSubscription.java
@@ -5,6 +5,7 @@ import naumen.framework.base.elements.Button;
 import naumen.framework.base.elements.CheckBox;
 import naumen.framework.base.elements.Label;
 import naumen.framework.forms.MainForm;
+import naumen.framework.forms.UserProfileForm;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -15,6 +16,8 @@ import java.util.List;
 public class LOTSLotSubscription extends NaumenTest {
 
     private MainForm mf;
+    private UserProfileForm upf;
+    private String lotName = "2.1 Проведение исследований в рамках международного многостороннего и двустороннего сотрудничества";
 
     @Parameters({"email", "password", "firstName", "lastName"})
     public LOTSLotSubscription(String mail, String psw, String nam, String snam){
@@ -41,30 +44,41 @@ public class LOTSLotSubscription extends NaumenTest {
             doAssert(subscrBtns.size() == 0, "Кнопка 'Подписаться' отсутствует", "Кнопка 'Подписаться' присутствует");
             makeScreen();
 
-        logStep("2", "LOT-S-1	Подписаться на новые конкурсы через форму подписки");
+        logStep("2", "LOT-S-2	Подписаться на новые конкурсы через форму подписки");
             login(user);
             mf = new MainForm();
             mf.financeLblClick();
             mf.lotSubscriptionClick();
             mf.checkTextOnForm("Подписка на конкурсы", this);
-            CheckBox cb = new CheckBox(By.xpath("//label[contains(.,'2.1 Проведение исследований в рамках международного многостороннего и двустороннего сотрудничества')]/input[@type='checkbox']"),
-                    "2.1 Проведение исследований в рамках международного многостороннего и двустороннего сотрудничества");
+            CheckBox cb = new CheckBox(By.xpath(String.format("//label[contains(.,'%s')]/input[@type='checkbox']", lotName)), lotName);
             if (!cb.isChecked())
                 cb.check();
             Button subscrBtn = new Button(By.xpath("//button[@type='submit' and contains(.,'Подписаться')]"), "Подписаться");
             subscrBtn.click();
             mf.checkTextOnForm("Вы успешно подписались на уведомления о появлении новых конкурсов", this);
-            mf.checkTextOnForm("2.1 Проведение исследований в рамках международного многостороннего и двустороннего сотрудничества", this);
+            mf.checkTextOnForm("Вы подписаны на следующие программные мероприятия:", this);
+            mf.checkTextOnForm(lotName, this);
+            makeScreen();
+
+            UserProfileForm.openUserProfile();
+            upf = new UserProfileForm();
+            String classAttr = upf.lots.getElement().getAttribute("class");
+            doAssert(classAttr.equals("active"), "Атрибут class вкладки 'Конкурсы' имеет значение 'active'", String.format("Атрибут class вкладки 'Конкурсы' имеет значение '%s'", classAttr));
+            upf.subscriptions.click();
+            String upperSubscrText = upf.getSubscriptionsList().get(0).getText();
+            doAssert(upperSubscrText.contains(lotName), "Верхней в списке отображается подписка с нужным названием", String.format("Верхней в списке отображается подписка с названием '%s'", upperSubscrText));
+            String subscrType = upf.getSubscrType(upf.getSubscriptionsList().get(0));
+            doAssert(subscrType.equals("Новые конкурсы"), "Тип подписки 'Новые конкурсы'", String.format("Тип подписки '%s'", subscrType));
             makeScreen();
             logout();
 
-        logStep("3", "LOT-S-1	Подписаться на новые конкурсы через профилирование");
+        logStep("3", "LOT-S-3	Подписаться на новые конкурсы через профилирование");
             login(user);
             mf.financeLblClick();
             mf.financeSearchClick();
             mf.checkTextOnForm("Поиск финансирования", this);
-            Label lab = new Label(By.xpath("//div[@class='title_small' and contains(.,'2.1 Проведение исследований в рамках международного многостороннего и двустороннего сотрудничества')]"),
-                    "2.1 Проведение исследований в рамках международного многостороннего и двустороннего сотрудничества");
+            Label lab = new Label(By.xpath(String.format("//div[@class='title_small' and contains(.,'%s')]", lotName)),
+                    lotName);
 
     }
 }
